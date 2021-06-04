bd732c70e16b3cd137d2b5eb3b5338fdca1f0e5f
197PM31180 - BÙI HOÀNG NAM - K25T-PM1 <nam.197pm31180@vanlanguni.vn>
2021-06-04 13:07:01 +0700

    ChiTietDonHang

---
+++ QuanLiNhaHang/Controllers/ChiTietDonHangController.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLiNhaHang.Models;
+
+namespace QuanLiNhaHnag.Controllers
+{
+    public class ChiTietDonHangsController : Controller
+    {
+        private CT25Team111Entities db = new CT25Team111Entities();
+        [Authorize(Roles = "Admin")]
+        // GET: ChiTietDonHangs
+        public ActionResult Index()
+        {
+            var model = db.ChiTietDonHangs.ToList();
+            return View(model);
+        }
+
+        public ActionResult Search(string keyword)
+        {
+            var model = db.ChiTietDonHangs.ToList();
+            model = model.Where(p => p.MaDH.ToString().Contains(keyword)).ToList();
+            ViewBag.Keyword = keyword;
+            return View("Index", model);
+
+        }
+
+        // GET: ChiTietDonHangs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietDonHang chiTietDonHang = db.ChiTietDonHangs.Find(id);
+            if (chiTietDonHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chiTietDonHang);
+        }
+
+        // GET: ChiTietDonHangs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaDH = new SelectList(db.DonHangs, "MaDH", "Email");
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "MaLoaiSP");
+            return View();
+        }
+
+        // POST: ChiTietDonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Ma_CTDH,Ma_ĐH,Ma_SP,Số_lượng")] ChiTietDonHang chiTietDonHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ChiTietDonHangs.Add(chiTietDonHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaDH = new SelectList(db.DonHangs, "MaDH", "Email", chiTietDonHang.MaDH);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "MaLoaiSP", chiTietDonHang.MaSP);
+            return View(chiTietDonHang);
+        }
+
+        // GET: ChiTietDonHangs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChiTietDonHang chiTietDonHang = db.ChiTietDonHangs.Find(id);
+            if (chiTietDonHang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaDH = new SelectList(db.DonHangs, "MaDH", "Email", chiTietDonHang.MaDH);
+            ViewBag.MaSP = new SelectList(db.SanPhams, "MaSP", "MaLoaiSP", chiTietDonHang.MaSP);
+            return View(chiTietDonHang);
+        }
+
+        // POST: ChiTietDonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaCTDH,MaDH,MaSP,SoLuong")] ChiTietDonHang chiTietDonHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chiTietDonHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaDH = new SelectList(db.DonHangs, "MaDH", "Email", chiTietDonHang.MaDH);
+            ViewBag.TenSP = new SelectList(db.SanPhams, "MaSP", "TenSP", chiTietDonHang.SanPham.TenSP);
+            return View(chiTietDonHang);
+        }
+
+        // GET: ChiTietDonHangs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var model = db.ChiTietDonHangs.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: ChiTietDonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var chiTietDonHang = db.ChiTietDonHangs.Find(id);
+            db.ChiTietDonHangs.Remove(chiTietDonHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
+namespace QuanLiNhaHang.Controllers
+{
+    public class DonHangsController : Controller
+    {
+        private CT25Team111Entities db = new CT25Team111Entities();
+        [Authorize(Roles = "Admin")]
+        // GET: DonHangs
+        public ActionResult Index()
+        {
+            var model = db.DonHangs.ToList();
+            return View(model);
+        }
+
+        public ActionResult Search(string keyword)
+        {
+            var model = db.DonHangs.ToList();
+            model = model.Where(p => p.MaDH.ToString().Contains(keyword)
+                                || p.Email.ToLower().Contains(keyword.ToLower())).ToList();
+            ViewBag.Keyword = keyword;
+            return View("Index", model);
+        }
+
+        // GET: DonHangs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaTT = new SelectList(db.TrangThaiDonHangs, "MaTT", "TrangThai");
+            return View();
+        }
+
+        // POST: DonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDH,Email,MaTT,DiaChiNguoiNhan,SDT")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DonHangs.Add(donHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaTT = new SelectList(db.TrangThaiDonHangs, "MaTT", "TrangThai", donHang.MaTT);
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaTT = new SelectList(db.TrangThaiDonHangs, "MaTT", "TrangThai", donHang.MaTT);
+            return View(donHang);
+        }
+
+        // POST: DonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDH,Email,MaTT,DiaChiNguoiNhan,SDT,TongTien")] DonHang donHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaTT = new SelectList(db.TrangThaiDonHangs, "MaTT", "TrangThai", donHang.MaTT);
+            return View(donHang);
+        }
+
+        // GET: DonHangs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: DonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DonHang donHang = db.DonHangs.Find(id);
+            db.DonHangs.Remove(donHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
+
+
